5f4e2e70a91011d9b95cd0fdf8094fd78c45d09a
Murodov28 <murodov2808@gmail.com>
2024-12-12 13:23:52 +0500

    my 4 commit

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,7 +4,7 @@ import Home from "../views/Home.vue";
 export const routers = createRouter({
 	history:createWebHistory(),
 	routes:[
-		{path:'/',name:'Home',component:Home},
+		{path:'/emarket',name:'Home',component:Home},
 		{path:'/Katalog',name:'Katalog',component:() => import('../views/Katalog.vue') },
 		{path:'/Korzina',name:'Korzina',component:() => import('../views/Korzina.vue') },
 		{path:'/AboutMe',name:'AboutMe',component:() => import('../views/AboutMe.vue') },
