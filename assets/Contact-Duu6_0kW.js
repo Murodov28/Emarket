28011fb1ee16ac9bb0bc26eed118ef3a56637325
Murodov28 <murodov2808@gmail.com>
2024-12-12 13:33:35 +0500

    Updates

---
+++ assets/Contact-Duu6_0kW.js
@@ -0,0 +1,1 @@
+import{_ as e,N as s,L as r,b as _,c as p,d as o,o as i,e as t}from"./index-BDX2_R5r.js";const l={components:{Navbar:s,Location:r,Footer:_}};function m(d,f,u,v,N,b){const n=t("Navbar"),a=t("Location"),c=t("Footer");return i(),p("div",null,[o(n),o(a,{class:"location__contact"}),o(c)])}const $=e(l,[["render",m]]);export{$ as default};
